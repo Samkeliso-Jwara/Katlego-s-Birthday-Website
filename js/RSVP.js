0dd7e6de98f1ac3b9ac72784546cbcb8bcad4398
Bongani <152927680+ThonoTheLord@users.noreply.github.com>
2024-01-25 12:21:45 +0200

    cleaned up some places and created an app

---
+++ js/RSVP.js
@@ -0,0 +1,24 @@
+function submitRSVP() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const attendance = document.getElementById('attendance').value;
+
+    // Generate unique QR code data
+    const qrCodeData = `${name},${email},${attendance}`;
+
+    // Create QR code
+    const qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: qrCodeData,
+        width: 128,
+        height: 128
+    });
+
+    // Download QR code as an image
+    const qrCodeImage = document.getElementById("qrcode").getElementsByTagName("img")[0];
+    const link = document.createElement('a');
+    link.href = qrCodeImage.src;
+    link.download = 'RSVP_QR_Code.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
